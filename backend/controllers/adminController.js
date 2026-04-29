9d76f236e6f6af5976723c4985625f9885171444
XKARTIKRAWAT <kartikrawat1333@gmail.com>
2026-04-29 12:03:17 +0530

    hardcode admin credentials for test

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,21 +1,13 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 export const adminLogin = (req, res) => {
   const { username, password } = req.body;
-
-  if (
-    username === process.env.ADMIN_USERNAME &&
-    password === process.env.ADMIN_PASSWORD
-  ) {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+  if (username === "admin" && password === "admin123") {
+    const token = jwt.sign({ username }, "secret123", {
       expiresIn: "7d",
     });
-
     return res.json({ success: true, token });
   }
-
   return res.status(401).json({ success: false, message: "Invalid credentials" });
 };
